00f7d4e9bd81e82787b69dcfcc7107e781a2ff84
bauti <bauti@DESKTOP-BV2C7C2>
2022-10-14 14:49:20 -0500

    Agregar archivos de proyecto.

---
+++ CanvasServidor/Services/ServidorCanvasServices.cs
@@ -0,0 +1,81 @@
+﻿using CanvasServidor.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace CanvasServidor.Services
+{
+    public class ServidorCanvasServices
+    {
+        TcpListener server;
+        public void IniciarServer()
+        {
+            if (server == null)
+            {
+                IPEndPoint IPE = new IPEndPoint(IPAddress.Any, 4999);
+                server = new TcpListener(IPE);
+                Thread hilo = new Thread(new ThreadStart(Escuchar));
+                hilo.IsBackground = true;
+                hilo.Start();
+            }
+        }
+
+        private void Escuchar()
+        {
+            if (server != null)
+            {
+                server.Start();
+                while (server.Server.IsBound)
+                {
+                    var clienteNuevo = server.AcceptTcpClient();
+                    Thread hiloRecibir = new Thread(new ParameterizedThreadStart(Recibir));
+                    hiloRecibir.Start(clienteNuevo);
+                }
+            }
+        }
+        public Rectangulo? r;
+        public event Action<Rectangulo> RectanguloNuevo;
+        private void Recibir(object? tcpClient)
+        {
+            if (tcpClient != null)
+            {
+                TcpClient cliente = (TcpClient)tcpClient;
+                var steam = cliente.GetStream();
+                while (cliente.Connected)
+                {
+                    if (cliente.Available > 0)
+                    {
+                        byte[] buffer = new byte[cliente.Available];
+                        steam.Read(buffer, 0, buffer.Length);
+                        try
+                        {
+                            var rectangulo = JsonConvert.DeserializeObject<Rectangulo>(System.Text.Encoding.UTF8.GetString(buffer));
+                            if (rectangulo != null)
+                            {
+                                r = new Rectangulo()
+                                {
+                                    Alto = rectangulo.Alto,
+                                    Ancho = rectangulo.Ancho,
+                                    CordenadaX = rectangulo.CordenadaX,
+                                    CordenadaY = rectangulo.CordenadaY,
+                                    Fill = rectangulo.Fill
+                                };
+                                RectanguloNuevo?.Invoke(r);
+                            }
+                        }
+                        catch (Exception){ }
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ CanvasServidor/ViewModels/CanvasViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CanvasServidor.Models;
+using CanvasServidor.Services;
+using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Color = System.Windows.Media.Color;
+using ColorConverter = System.Windows.Media.ColorConverter;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace CanvasServidor.ViewModels
+{
+    public class CanvasViewModel
+    {
+        public ICommand IniciarCommand { get; set; }
+        MainWindow? canvas;
+        Dispatcher dispatcher;
+        ServidorCanvasServices scs;
+        public CanvasViewModel()
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+            IniciarCommand = new RelayCommand(IniciarServidor);
+        }
+        void IniciarServidor()
+        {
+            scs = new ServidorCanvasServices();
+            scs.RectanguloNuevo += Scs_RectanguloNuevo;
+            scs.IniciarServer();
+        }
+        private void Scs_RectanguloNuevo(Rectangulo obj)
+        {
+            dispatcher.Invoke(() =>
+            {
+                canvas = (MainWindow?)Application.Current.Windows.OfType<Window>().FirstOrDefault();
+                Color fill = (Color)ColorConverter.ConvertFromString(scs.r.Fill);
+                Brush brush = new SolidColorBrush(fill);
+                if (canvas != null)
+                {
+                    Rectangle rec = new Rectangle()
+                    {
+                        Width = scs.r.Ancho,
+                        Height = scs.r.Alto,
+                        Fill = brush
+                    };
+                    canvas.canvasserver.Children.Add(rec);
+                    Canvas.SetTop(rec, scs.r.CordenadaY);
+                    Canvas.SetLeft(rec, scs.r.CordenadaX);
+                }
+            });
+        }
+    }
+}
